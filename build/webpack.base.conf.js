25c1dae0dcd6059f91afa7867564d604abd3f464
adypr <workady2015@gmail.com>
2019-04-19 11:29:10 +0300

    file-loader,copy-webpack-plugin,html-webpack-pl...

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,20 +1,37 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/'
+}
 
 module.exports = {
+    externals: {
+        paths: PATHS
+    },
     entry: {
-        app: './src/index.js'
+        app: PATHS.src
     },
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist'
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/'
     },
     module: {
         rules: [{
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
+        }, {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]'
+            }
         }, {
             test: /\.scss$/,
             use: [
@@ -25,7 +42,7 @@ module.exports = {
                     options: { sourceMap: true }
                 }, {
                     loader: 'postcss-loader',
-                    options: { sourceMap: true, config: { path:'src/js/postcss.config.js' } }
+                    options: { sourceMap: true, config: { path:`${PATHS.src}/js/postcss.config.js` } }
                 }, {
                     loader: 'sass-loader',
                     options: { sourceMap: true }
@@ -41,14 +58,23 @@ module.exports = {
                     options: { sourceMap: true }
                 }, {
                     loader: 'postcss-loader',
-                    options: { sourceMap: true, config: { path:'src/js/postcss.config.js' } }
+                    options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
                 }
             ]
         }]
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "[name].css"
-        })
+            filename: `${PATHS.assets}css/[name].css`,
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATHS.src}/index.html`,
+            filename: './index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
+            { from: `${PATHS.src}/static`, to: '' },
+        ])
     ]
 }
